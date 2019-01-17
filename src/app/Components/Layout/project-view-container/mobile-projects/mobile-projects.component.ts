17bb3760537fe0ff1f6bc592226663ab87f16ea5
Chris Anderson <chrisaaxx@gmail.com>
2019-01-17 12:55:46 -0500

    final

--- src/app/Components/Layout/project-view-container/mobile-projects/mobile-projects.component.ts
+++ src/app/Components/Layout/project-view-container/mobile-projects/mobile-projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterViewChecked, Input } from '@angular/core';
 import { ColorSwitchComponent } from './color-switch/color-switch.component';
 import { MatDialog } from '@angular/material';
 import { SourceCodeService } from 'src/app/Services/source-code.service';
@@ -6,56 +6,48 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { SpaceGameComponent } from './space-game/space-game.component';
 import { FlashChatComponent } from './flash-chat/flash-chat.component';
 import { SceneKitComponent } from './scene-kit/scene-kit.component';
-import { transition, trigger, query, style, stagger, animate, keyframes } from '@angular/animations';
+import { transition, trigger, query, style, stagger, animate, keyframes, sequence } from '@angular/animations';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { mobileProjects } from 'src/app/Models/Projects';
-
+export const ROUTE_ANIMATIONS_ELEMENTS = 'route-animations-elements';
 @Component({
   selector: 'app-mobile-projects',
   templateUrl: './mobile-projects.component.html',
   styleUrls: ['./mobile-projects.component.css'],
   animations: [
-    trigger('listStagger', [
-      transition('* <=> *', [
-        query(
-          ':enter',
-          [
-            style({ opacity: 0, transform: 'translateY(-15px)' }),
-            stagger(
-              '50ms',
-              animate(
-                '550ms ease-out',
-                style({ opacity: 1, transform: 'translateY(0px)' })
-              )
-            )
-          ],
-          { optional: true }
-        ),
-        query(':leave', animate('50ms', style({ opacity: 0 })), {
-          optional: true
-        })
+    trigger('listAnimation', [
+      transition('*<=>*', [
+        query(':enter', [
+          style({ opacity: 0, transform: 'translateY(-200px)' }),
+          stagger(100, [
+            animate('500ms 400ms cubic-bezier(0.35, 0, 0.25, 1)',
+              style({ opacity: 1, transform: 'none' }))
+          ])
+        ]),
+        // query(':leave', [
+        //   style({ opacity: 1, transform: 'none' }),
+        //   stagger(-100, [
+        //     animate('10ms cubic-bezier(0.35, 0, 0.25, 1)',
+        //       style({ opacity: 0, transform: 'translateY(+200px)' }))
+        //   ])
+        // ])
       ])
-    ])
+    ]),
   ]
-  // animations: [
-  //   trigger('cardAnimations', [
-  //     transition('* => *', [
-  //       query('img', style({ transform: 'translateY(-100%)' })),
-  //       query('img',
-  //         stagger('2000ms', [
-  //           animate('2000ms', style({ transform: 'translateY(-100%)' }))
-  //         ]))
-  //     ])
-  //   ])
-  // ]
+
 })
 export class MobileProjectsComponent implements OnInit {
+  projects;
 
-  projects = mobileProjects;
 
-  constructor(private dialog: MatDialog, private sourceCodeService: SourceCodeService) { }
+  constructor(private dialog: MatDialog, private service: SourceCodeService) { }
 
   ngOnInit() {
+    this.service.currentProjectView.subscribe(projects => this.projects = projects);
+  }
+
+  addProjects() {
+    // this.projects = mobileProjects;
   }
 
   openDialog(component: string) {
